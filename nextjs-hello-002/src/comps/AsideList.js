de9c6f2bf68a18f750a0eea46b844c209b7dc0a4
callor <callor@callor.com>
2024-02-29 15:02:08 +0900

    blog

--- nextjs-hello-002/src/comps/AsideList.js
+++ nextjs-hello-002/src/comps/AsideList.js
@@ -1,30 +1,30 @@
 "use client";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import { usePathname } from "next/navigation";
 import styles from "./asideList.module.css";
 export default () => {
-  const router = useRouter();
-  return (
-    <ul className={styles.ul}>
+  // Link, router 를 통해서 열렸을때 사용한 URL 을 알려주는 함수
+  const pathname = usePathname();
+
+  const routes = [
+    { path: "/blog/write", title: "글쓰기" },
+    { path: "/blog/all", title: "전체글 보기" },
+    { path: "/blog/new", title: "새글 보기" },
+    { path: "/blog/like", title: "인기글 보기" },
+  ];
+
+  const viewRoutes = routes.map((route) => {
+    return (
       <li>
         <Link
-          href="/blog/write"
-          className={
-            router.pathname === "/blog/write" ? styles.active : ""
-          }
+          href={route.path}
+          className={pathname === route.path ? styles.active : ""}
         >
-          글쓰기({router})
+          {route.title}
         </Link>
       </li>
-      <li>
-        <Link href="/blog/all">전체글 보기</Link>
-      </li>
-      <li>
-        <Link href="/blog/like">인기글 보기</Link>
-      </li>
-      <li>
-        <Link href="/blog/new">최신글 보기</Link>
-      </li>
-    </ul>
-  );
+    );
+  });
+
+  return <ul className={styles.ul}>{viewRoutes}</ul>;
 };
