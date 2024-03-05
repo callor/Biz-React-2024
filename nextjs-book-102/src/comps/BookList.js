0a51c4c7f4488302e8f339f76819f8c432c3a78a
callor <callor@callor.com>
2024-03-05 16:57:39 +0900

    nextjs

--- nextjs-book-102/src/comps/BookList.js
+++ nextjs-book-102/src/comps/BookList.js
@@ -1,6 +1,16 @@
+"use client";
+import { useEffect, useState } from "react";
 import { getNaverBooks } from "@/api/naver_api";
 const BookList = async ({ search }) => {
-  const books = await getNaverBooks(search);
+  const [books, setBooks] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await getNaverBooks(search);
+      setBooks([...result]);
+    };
+    fetchData();
+  }, []);
+
   const viewList = books.map((book) => <li>{book.title}</li>);
   return <ul>{viewList}</ul>;
 };
